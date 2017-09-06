feb584450ed6977f71e09f84e87f5d3c3523064f
tleb <tleb@openmailbox.org>
2017-09-06 22:49:14 +0200

    Fix markers' links to user profiles

--- docs/main.js
+++ docs/main.js
@@ -8,11 +8,11 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
 
-function userMarker(latlng, username, urlPath) {
+function userMarker(latlng, username, uri) {
   return L.marker(latlng, {
     title: username,
     alt:   username,
-  }).bindPopup(`<a href="https://zestedesavoir.com${urlPath}" class="popup-link">${username}</a>`)
+  }).bindPopup(`<a href="${uri}" class="popup-link">${username}</a>`)
 }
 
 function getMarkersData(cb) {
@@ -26,6 +26,6 @@ function getMarkersData(cb) {
 
 getMarkersData(function(markers) {
   var cluster = L.markerClusterGroup()
-  cluster.addLayers(markers.map(m => userMarker(m.latlng, m.username, m.urlPath)))
+  cluster.addLayers(markers.map(m => userMarker(m.latlng, m.username, m.uri)))
   map.addLayer(cluster)
 })
