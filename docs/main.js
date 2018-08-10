361d02ba2f6570d8542decc26eadc008aa6b7322
tleb <tleb@openmailbox.org>
2018-08-10 15:07:42 +0200

    Querystring search

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map', {
+const map = L.map('map', {
   center:  [35.317, -0.527],
   zoom:    3,
 })
@@ -15,24 +15,46 @@ function userMarker(latlng, username, uri) {
 }
 
 function getMarkersData(cb) {
-  var httpReq = new XMLHttpRequest()
+  const httpReq = new XMLHttpRequest()
   httpReq.addEventListener('load', function() {
     cb(JSON.parse(this.responseText))
   })
-  httpReq.open('GET', 'https://tleb.github.io/zds-user-map/markers.json', false)
+  httpReq.open('GET', 'https://tleb.github.io/zds-user-map/markers.json', true)
   httpReq.send()
 }
 
 getMarkersData(function(markers) {
-  var cluster = L.markerClusterGroup()
+  const cluster = L.markerClusterGroup()
   cluster.addLayers(markers.map(m => userMarker(m.latlng, m.username, m.uri)))
   map.addLayer(cluster)
 
-  map.addControl(new L.Control.Search({
+  const controlSearch = new L.Control.Search({
     position: 'topright',
     layer: cluster,
     initial: false,
     zoom: 12,
     marker: false,
-  }))
+  })
+  map.addControl(controlSearch)
+
+  const querystring = window.location.href.split('?')[1] || ''
+
+  // ignore '/?' and '/' cases
+  if (querystring === '') return;
+
+  // search the querystring
+  controlSearch.searchText(querystring)
+
+  // when something has been found, press enter
+  // only happens once
+  const observer = new MutationObserver(_ => {
+    controlSearch._handleKeypress({ keyCode: 13 })
+    observer.disconnect()
+  })
+  observer.observe(controlSearch._tooltip, { childList: true })
+
+  // if the user searches something, we should disable the auto-enter
+  document.querySelector('input').addEventListener('change', () => {
+    observer.disconnect()
+  })
 })
