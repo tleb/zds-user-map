6f7bcbd181d92820bf96cc5354aca0f4275f1771
tleb <tleb@openmailbox.org>
2018-12-25 20:43:14 +0100

    push initial data

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main(mode):
     if not (0 <= mode <= 2):
         return print('''Available modes:
 0: initialise the refresh token based on the username and password
-1: initialise the data (add every position to the database)
+1: initialise the data (add every marker)
 2: daemon mode (watch new topics)''')
 
     with open(CONFIG_PATH, encoding='UTF-8') as f:
@@ -38,69 +38,21 @@ def main(mode):
         zds._refresh_token_from_logins(input('Username: '), getpass())
 
     if mode == 1:
-        markers = []
-
         topics = list(zds.topics(False))
         topics.reverse()
 
-        bot_id = config['bot_id']
-        deletion_msg = config['deletion_msg']
-        for topic in topics:
-            msgs = list(zds.messages(topic['id']))
-            msgs.reverse()
-
-            pos, user_id = None, None
-
-            for msg in msgs:
-                if msg['author'] == bot_id:
-                    continue
-
-                if msg['text'] == deletion_msg:
-                    break
-
-                osm_ti.start()
-                pos = utils.get_pos(msg['text'])
-                if pos is not None:
-                    user_id = msg['author']
-                    break
-
-            if pos is None:
-                continue
-
-            user = zds.get_user(user_id)
-            marker = OrderedDict([
-                ('id', user_id),
-                ('username', user['username']),
-                ('url', ZesteDeSavoir.URI_BASE + user['html_url']),
-                ('lat', pos['lat']),
-                ('lon', pos['lon'])])
-
-            markers.append(marker)
-            break
-
+        markers = [utils.marker_from_topic(topic, zds, config, osm_ti)
+                    for topic in topics]
+        markers = [x for x in markers if x is not None]
         markers.sort(key=lambda m: m['id'])
 
         with open(MARKERS_PATH, 'w', encoding='UTF-8') as f:
             json.dump(markers, f, indent=2)
 
-
-
     if mode == 2:
-        print('mode 2')
+        pass
 
 
 
 if __name__ == '__main__':
     main(int(sys.argv[1]) if 1 < len(sys.argv) and sys.argv[1].isdigit() else -1)
-
-#from runner import Runner
-
-#runner = Runner('config.yml', 'store.json', 'docs/markers.json')
-#runner.run()
-
-
-# id
-# lat
-# lon
-# uri
-# username
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import urllib.parse
+from collections import OrderedDict
+from zeste_de_savoir import ZesteDeSavoir
 
 
 OSM_SEARCH = 'http://nominatim.openstreetmap.org/search?format=json&q={}'
@@ -29,3 +31,33 @@ def last_text_position(msgs, bot_id, deletion_msg):
             last_position = msg['text']
 
         return last_position
+
+def marker_from_topic(topic, zds, config, osm_ti):
+    msgs = list(zds.messages(topic['id']))
+    msgs.reverse()
+
+    pos, user_id = None, None
+
+    for msg in msgs:
+        if msg['author'] == config['bot_id']:
+            continue
+
+        if msg['text'] == config['deletion_msg']:
+            break
+
+        osm_ti.start()
+        pos = get_pos(msg['text'])
+        if pos is not None:
+            user_id = msg['author']
+            break
+
+    if pos is None:
+        return None
+
+    user = zds.get_user(user_id)
+    return OrderedDict([
+        ('id', user_id),
+        ('username', user['username']),
+        ('url', ZesteDeSavoir.URI_BASE + user['html_url']),
+        ('lat', pos['lat']),
+        ('lon', pos['lon'])])
